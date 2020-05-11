a8b802f0add8f0758d6117706fb371d2cc77b446
Guillaume Millot <guillaume.millot@gmail.com>
2020-05-11 16:13:35 +0200

    Initial commit

---
+++ tests/test_01.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <caesar.h>
+#include <gen_test.h>
+
+int main(void)
+{
+    printf("Starting...\n");
+    
+    check_magic_number();
+
+    char str[] = "People of Earth";
+
+    printf("Test 1: Shift with a negative input\n");
+    gen_test(str, -3);
+    printf("\nTest 2: Shift with a positive input\n");
+    gen_test(str, 7);
+
+    return 0;
+}
